9ee225c07b1e93ac9401dbed0d7992ca2001f2d0
swatidurgekar <swatidurgekar@gmail.com>
2023-11-19 17:55:53 +0530

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get("/", (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: "Added todo", todo: newTodo, todos: todos });
+});
+router.put("/todo/:todoId", (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        return res.status(200).json({ message: "updated todo", todos: todos });
+    }
+    res.status(404).json({ message: "Could not find todo for this id." });
+});
+router.delete("/todo/:todoId", (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todo) => todo.id === req.params.todoId);
+    if (todoIndex >= 0) {
+        todos = todos.filter((todoItem) => todoItem.id !== tid);
+        return res.status(200).json({ message: "Delete todo", todos: todos });
+    }
+    res.status(404).json({ message: "Could not find todo for this id." });
+});
+exports.default = router;
